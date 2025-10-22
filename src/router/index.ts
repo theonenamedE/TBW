90159f9468f3ffa3f249c8573206b873f59d17f3
Mohammad Mahdi <mmm.6831.allah@gmail.com>
2025-10-22 15:22:56 +0330

    Performance optimizations

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,35 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import NotFoundView from '@/views/NotFoundView.vue'
-import ListView from '@/views/ListView.vue'
-import DetailsView from '@/views/DetailsView.vue'
-import AddView from '@/views/AddView.vue'
-import WatchView from '@/views/WatchView.vue'
 
 const routes: RouteRecordRaw[] = [
-  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
+  { 
+    path: '/:pathMatch(.*)*', 
+    name: 'NotFound', 
+    component: () => import('@/views/NotFoundView.vue'),
+  },
   {
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: () => import('@/views/HomeView.vue'),
   },
   {
     path: '/list',
     name: 'list',
-    component: ListView,
+    component: () => import('@/views/ListView.vue'),
   },
   {
     path: '/details/:id',
     name: 'details',
-    component: DetailsView,
+    component: () => import('@/views/DetailsView.vue'),
   },
   {
     path: '/add',
     name: 'add',
-    component: AddView,
+    component: () => import('@/views/AddView.vue'),
   },
   {
     path: '/watch/:name/:id',
     name: 'watch',
-    component: WatchView,
+    component: () => import('@/views/WatchView.vue'),
   },
 ]
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+//import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/TBW/' : './',
   plugins: [
-    vue(),  
+    vue(),
     vueDevTools(),
-    tailwindcss()
+    tailwindcss(),
+    //analyzer()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
